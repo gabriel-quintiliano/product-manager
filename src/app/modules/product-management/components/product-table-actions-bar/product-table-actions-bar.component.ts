e3ba4bbe15fee8f2a7d5957664c22fc0e14a4428
Gabriel Quintiliano <gabrielq.pereira@gmail.com>
2023-11-15 20:27:57 -0300

    Rename some components from list to table to be more accurate

---
+++ src/app/modules/product-management/components/product-table-actions-bar/product-table-actions-bar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'pmng-product-table-actions-bar',
+	templateUrl: './product-table-actions-bar.component.html',
+	styleUrls: ['./product-table-actions-bar.component.css']
+})
+export class ProductTableActionsBarComponent {
+
+}
--- src/app/modules/product-management/product-management.module.ts
+++ src/app/modules/product-management/product-management.module.ts
@@ -1,33 +1,33 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductComponent } from './components/product/product.component';
-import { ProductListComponent } from './components/product-list/product-list.component';
+import { ProductTableComponent } from './components/product-table/product-table.component';
 import { ProductListHeaderComponent } from './components/product-list-header/product-list-header.component';
-import { ProductListActionsBarComponent } from './components/product-list-actions-bar/product-list-actions-bar.component';
+import { ProductTableActionsBarComponent } from './components/product-table-actions-bar/product-table-actions-bar.component';
 import { ProductEditActionsBarComponent } from './components/product-edit-actions-bar/product-edit-actions-bar.component';
 import { ProductEditFormComponent } from './components/product-edit-form/product-edit-form.component';
 
 
 
 @NgModule({
-  declarations: [
-    ProductComponent,
-    ProductListComponent,
-    ProductListHeaderComponent,
-    ProductListActionsBarComponent,
-    ProductEditActionsBarComponent,
-    ProductEditFormComponent
-  ],
-  imports: [
-    CommonModule
-  ],
-  exports: [
-    ProductComponent,
-    ProductListComponent,
-    ProductListHeaderComponent,
-    ProductListActionsBarComponent,
-    ProductEditActionsBarComponent,
-    ProductEditFormComponent
-  ]
+	declarations: [
+		ProductComponent,
+		ProductTableComponent,
+		ProductListHeaderComponent,
+		ProductTableActionsBarComponent,
+		ProductEditActionsBarComponent,
+		ProductEditFormComponent
+	],
+	imports: [
+		CommonModule
+	],
+	exports: [
+		ProductComponent,
+		ProductTableComponent,
+		ProductListHeaderComponent,
+		ProductTableActionsBarComponent,
+		ProductEditActionsBarComponent,
+		ProductEditFormComponent
+	]
 })
 export class ProductManagementModule { }
