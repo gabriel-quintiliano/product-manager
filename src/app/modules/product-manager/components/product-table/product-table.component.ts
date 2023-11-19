fbbf4db8f4c001586d1b81f24e7d35faa3755a15
Gabriel Quintiliano <gabrielq.pereira@gmail.com>
2023-11-19 14:59:14 -0300

    Implement methods addData and deleteSelected to component product-table

--- src/app/modules/product-manager/components/product-table/product-table.component.ts
+++ src/app/modules/product-manager/components/product-table/product-table.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { SelectionModel } from '@angular/cdk/collections';
-import { MatTableDataSource } from '@angular/material/table';
+import { MatTable, MatTableDataSource } from '@angular/material/table';
 import { Product, ProductDefaults } from '../../models/product.model';
 
 @Component({
@@ -10,11 +10,14 @@ import { Product, ProductDefaults } from '../../models/product.model';
 })
 
 export class ProductTableComponent {
+
+	@ViewChild('productsTable') productsTable!: MatTable<Product>;
 	productDefaults = ProductDefaults; // declared here so that I can use it in the components template
 	displayedColumns = ['select', ...Object.keys(ProductDefaults)];
 
 	products = new MatTableDataSource<Product>(PRODUCTS_SAMPLE);
 	selection = new SelectionModel<Product>(true, []);
+	indexesSelected: number[] = []
 
 	/** Whether the number of selected elements matches the total number of rows. */
 	isAllSelected() {
@@ -40,11 +43,40 @@ export class ProductTableComponent {
 		}
 		return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ?`;
 	}
+
+	addData(data: Product) {
+		this.products.filteredData.push(data)
+		this.productsTable.renderRows()
+	}
+
+	deleteSelected() {
+
+		if (!this.selection.selected) {
+			return;
+		}
+
+		for (let elem of this.selection.selected) {
+			for (let i = 0; i < this.products.filteredData.length; i++) {
+				if (elem === this.products.filteredData[i]) {
+					this.products.filteredData.splice(i, 1);
+				}
+			}
+		}
+
+		this.selection.clear()
+		this.productsTable.renderRows()
+	}
+
+	// for testing
+	// mostrar() {
+	// 	this.addData(new Product('sku-CAR-23-mca', 'Porsche Macan Turbo - 2023', 478000, 130244, 20, 123444, 899990))
+	// }
 }
 
 const PRODUCTS_SAMPLE: Product[] = [ // for testing initially
+	new Product('sku-CAR-23-mca', 'Porsche Macan Turbo - 2023', 478000, 130244, 20, 123444, 899990),
 	new Product("SKU-TV-29-3787", "TV 59\" LCD 4K - Smart Connection", 4300, 400, 1000, 1.89, 5700),
-	new Product("SKU-TV-29-3787", "TV 59\" LCD 4K - Smart Connection", 4300, 400, 1000, 1.89, 5700),
-	new Product("SKU-TV-29-3787", "TV 59\" LCD 4K - Smart Connection", 4300, 400, 1000, 1.89, 5700),
-	new Product("SKU-TV-29-3787", "TV 59\" LCD 4K - Smart Connection", 4300, 400, 1000, 1.89, 5700)
+	new Product("SKU-NOTE-29-3787", "NOTE 59\" LCD 4K - Smart Connection", 4300, 400, 1000, 1.89, 5700),
+	new Product("SKU-PSP-29-3787", "PSP 59\" LCD 4K - Smart Connection", 4300, 400, 1000, 1.89, 5700),
+	new Product("SKU-XBOX-29-3787", "XBOX 59\" LCD 4K - Smart Connection", 4300, 400, 1000, 1.89, 5700)
 ]
