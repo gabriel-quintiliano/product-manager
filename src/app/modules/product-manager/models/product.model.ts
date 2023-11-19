a16f2a22dae533f97d82b8ed3fed1c4a1e9b0cb7
Gabriel Quintiliano <gabrielq.pereira@gmail.com>
2023-11-19 15:56:23 -0300

    Refactor model Product within module product-manager

--- src/app/modules/product-manager/models/product.model.ts
+++ src/app/modules/product-manager/models/product.model.ts
@@ -1,13 +1,21 @@
-type ProductKeyIdentifier = "sku" | "name" | "costPrice" | "relatedExpenses" | "markup" | "margin" | "sellingPrice"
+type IProduct = {
+	sku: string,
+	name: string,
+	costPrice: number,
+	relatedExpenses: number,
+	markup: number,
+	margin: number,
+	sellingPrice: number,
+}
 
-type ProductDefaultsType = {
-	[key in ProductKeyIdentifier]: {
+type ProductFeaturesMetadata = {
+	[key in keyof IProduct]: {
 		label: string,
 		description: string
 	};
 }
 
-const ProductDefaults: ProductDefaultsType = {
+const ProductMetadata: ProductFeaturesMetadata = {
 	sku: { label: "sku", description: "Código exclusivo de um produto utilizado para identificar o mesmo com base em suas características" },
 	name: { label: "nome", description: "Nome do produto que aparecerá no site da loja" },
 	costPrice: { label: "preço de custo", description: "Preço pago para adquirir o produto do fornecedor/fabricá-lo" },
@@ -17,31 +25,16 @@ const ProductDefaults: ProductDefaultsType = {
 	sellingPrice: { label: "preço de venda", description: "Preço final pelo qual o produto será comercializado" }
 } as const;
 
-
-type ProductProperty<T> = {
-	value: T;
-	readonly label: string;
-	readonly description: string;
-}
-
-class Product {
-	sku: ProductProperty<string>;
-	name: ProductProperty<string>;
-	costPrice: ProductProperty<number>;
-	relatedExpenses: ProductProperty<number>;
-	markup: ProductProperty<number>;
-	margin: ProductProperty<number>;
-	sellingPrice: ProductProperty<number>;
-
-	constructor(sku: string, name: string, costPrice: number, relatedExpenses: number, markup: number, margin: number, sellingPrice: number) {
-		this.sku = { value: sku, ...ProductDefaults.sku };
-		this.name = { value: name, ...ProductDefaults.name };
-		this.costPrice = { value: costPrice, ...ProductDefaults.costPrice };
-		this.relatedExpenses = { value: relatedExpenses, ...ProductDefaults.relatedExpenses };
-		this.markup = { value: markup, ...ProductDefaults.markup };
-		this.margin = { value: margin, ...ProductDefaults.margin };
-		this.sellingPrice = { value: sellingPrice, ...ProductDefaults.sellingPrice };
+class Product implements IProduct {
+	constructor(
+		public sku: string,
+		public name: string,
+		public costPrice: number,
+		public relatedExpenses: number,
+		public markup: number,
+		public margin: number,
+		public sellingPrice: number) {
 	}
 }
 
-export { Product, ProductDefaults }
+export { Product, ProductMetadata }
