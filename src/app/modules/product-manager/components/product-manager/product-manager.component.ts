02b307463eeb8440716dc49ad204593f41c78afe
Gabriel Quintiliano <gabrielq.pereira@gmail.com>
2023-11-20 10:50:11 -0300

    Futher implement component product-manager in module product-manager

--- src/app/modules/product-manager/components/product-manager/product-manager.component.ts
+++ src/app/modules/product-manager/components/product-manager/product-manager.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, SimpleChanges, ViewChild } from '@angular/core';
+import { ProductTableComponent } from '../product-table/product-table.component';
+import { Action } from '../../models/action.model';
 
 @Component({
-  selector: 'pmg-product-manager',
-  templateUrl: './product-manager.component.html',
-  styleUrls: ['./product-manager.component.css']
+	selector: 'pmg-product-manager',
+	templateUrl: './product-manager.component.html',
+	styleUrls: ['./product-manager.component.css']
 })
 export class ProductManagerComponent {
+	@ViewChild("table") table!: ProductTableComponent;
+	numberOfSelectedItems: number = 0;
 
+	ngAfterViewInit() {
+		// Setting to make it possible that every time the number of selected
+		// items is needed, this value if fetched live from product-table component.
+		Object.defineProperty(this, 'numberOfSelectedItems', {
+			get() {
+				return this.table.selection.selected.length;
+			}
+		})
+	}
+
+	runAction(action: Action) {
+		console.log(`Action to be taken: ${action}Product()`)
+		eval(`this.${action}Product()`)
+	}
+
+	deleteProduct() {
+		this.table.deleteSelected()
+	}
+
+	addProduct() {
+
+	}
+
+	editProduct() {
+
+	}
 }
